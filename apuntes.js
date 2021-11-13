a4fad2cdac915d51995d9dfb169d3f9e1f9c0471
joacocutropia <joacocutropia@gmail.com>
2021-11-13 20:48:13 -0300

    se agrego apuntes.js

---
+++ apuntes.js
@@ -0,0 +1,12 @@
+/*
+function miFuncion(){}
+
+const otraFuncion = function(){}
+
+const yOtra =  => {}
+*/
+
+function presentar(nombre, edad){
+    console.log('Hola mi nombre es ' + nombre)
+    console.log('Tengo ' + edad + 'años')
+}
